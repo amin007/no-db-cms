664830c8a2f5b3157f0fc1b4fbb7225dff3e2ac6
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-06-18 12:22:51 +0800

    susun balik koding ikut gaya amin ledang

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 	$page_slug = (isset($_GET['page'])) ? trim($_GET['page']) : 'home';
+
 	if ($page_slug == '') { $page = 'home'; }
 
 	$page = "pages/$page_slug.php";
 
-	if (!file_exists($page)) {
+	if (!file_exists($page))
+	{
 		$page = 'pages/404.php';
 	}
 
@@ -12,45 +14,46 @@
 
 ?><!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
 
-    <title>No DB CMS</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-
+<title>No DB CMS</title>
+<!-- Bootstrap core CSS -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet">
 <style>
-	main {
-		padding-top: 30px;
-		padding-bottom: 100px;
-		min-height: 400px;
-		background-color: #fff;
-	}
+main 
+{
+	padding-top: 30px;
+	padding-bottom: 100px;
+	min-height: 400px;
+	background-color: #fff;
+}
 	
-	body {
-		background-color: #333;
-	}
+body
+{
+	background-color: #333;
+}
 	
-	footer {
-		background-color: #333;
-		color: #fff;
-	}
+footer
+{
+	background-color: #333;
+	color: #fff;
+}
 	
-	#sidebar  {
-		border-right: 1px dotted #ccc;
-	}
+#sidebar
+{
+	border-right: 1px dotted #ccc;
+}
 </style>
-	</head>
-
-  <body>
+</head>
+<body>
     
 <!-- As a heading -->
 <nav class="navbar navbar-light bg-light">
-  <span class="navbar-brand mb-0 h1">NODBCMS</span>
+<span class="navbar-brand mb-0 h1">NODBCMS</span>
 </nav>    
 <main>
 <div class="container-fluid">
@@ -58,16 +61,19 @@
 	<div class="row">
 		<div class="col-md-3 col-lg-2" id="sidebar">
 			<ul class="nav flex-column nav-pills">
-		  <?php foreach($pages as $m) {
-				$active = '';
-				if ($m['page_slug'] == $page_slug) {
-					$active = ' active';
-				}
-				?>
-			  <li class="nav-item">
-				<a class="nav-link<?php echo $active;?>" href="index.php?page=<?php echo $m['page_slug'];?>"><?php echo $m['label'];?></a>
-			  </li>		  
-		  <?php } ?>
+<?php foreach($pages as $m)
+{
+	$active = '';
+	if ($m['page_slug'] == $page_slug)
+	{
+		$active = ' active';
+	}
+	echo "\t\t\t";
+	?><li class="nav-item">
+				<a class="nav-link<?php echo $active;?>" href="index.php?page=<?php
+				echo $m['page_slug'];?>"><?php echo $m['label'];?></a>
+			</li>
+<?php } ?>
 			</ul>
 		</div>
 		<div class="col-md-9 col-lg-10">
@@ -77,26 +83,23 @@
 		?>
 		</div>
 	</div>
-	
 </div>
-	  </main>
+</main>
 	  
-<footer>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-12">
-				<p>Hakcipta terpelihara</p>
-			</div>
+<footer><div class="container-fluid">
+	<div class="row">
+		<div class="col-12">
+			<p>Hakcipta terpelihara</p>
 		</div>
 	</div>
-</footer>
+</div></footer>
    
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
 
-  </body>
+</body>
 </html>
